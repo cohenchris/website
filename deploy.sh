123b9d918017b14550052136bc50108e9320911e
cohenchris <40076937+cohenchris@users.noreply.github.com>
2023-10-18 15:37:59 -0700

    border around resume, made resume higher quality, and tweaked navLinks to align them to the right side

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ python3 ./website-listening-progress-json.py
 
 # Get Resume JPG, convert to webp, and resize
 wget https://raw.githubusercontent.com/cohenchris/resume/master/ChrisCohen_resume.jpg
-cwebp -q 50 ChrisCohen_resume.jpg -o static/images/ChrisCohen_resume.webp
+cwebp -q 80 ChrisCohen_resume.jpg -o static/images/ChrisCohen_resume.webp
 cwebp -resize 800 1131 static/images/ChrisCohen_resume.webp -o static/images/ChrisCohen_resume.webp
 rm ChrisCohen_resume.jpg
 
