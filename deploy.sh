6b937eea6fac8b912af10238352afe907fe98c78
cohenchris <40076937+cohenchris@users.noreply.github.com>
2023-10-18 14:41:10 -0700

    made resume image part of the website to speed up image loading

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,16 @@ cd "$(dirname "$0")"
 # Font subset
 ./get-all-chars.py space.ttf
 
+# Sync music listening progress metadata
 python3 ./website-listening-progress-json.py
 
+# Get Resume JPG, convert to webp, and resize
+wget https://raw.githubusercontent.com/cohenchris/resume/master/ChrisCohen_resume.jpg
+cwebp -q 50 ChrisCohen_resume.jpg -o static/images/ChrisCohen_resume.webp
+cwebp -resize 800 1131 static/images/ChrisCohen_resume.webp -o static/images/ChrisCohen_resume.webp
+rm ChrisCohen_resume.jpg
+
+# Generate and deploy to server
 hugo
 
 mv public/ html/
