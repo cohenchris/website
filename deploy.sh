b111f43a69fc8d24bc110b290ac7592a71a86c1f
cohenchris <40076937+cohenchris@users.noreply.github.com>
2023-10-18 16:20:32 -0700

    resized resume to save space

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,8 @@ cd "$(dirname "$0")"
 
 # Get Resume JPG, convert to webp, and resize
 wget https://raw.githubusercontent.com/cohenchris/resume/master/ChrisCohen_resume.jpg
-cwebp -q 25 ChrisCohen_resume.jpg -o static/images/ChrisCohen_resume.webp
+convert ChrisCohen_resume.jpg -resize 798x1128 ChrisCohen_resume.jpg
+cwebp -q 50 ChrisCohen_resume.jpg -o static/images/ChrisCohen_resume.webp
 rm ChrisCohen_resume.jpg
 
 # Sync music listening progress metadata
