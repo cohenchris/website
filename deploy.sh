633055d08d52e4285ca9d4d4b700504cf13c6c56
cohenchris <40076937+cohenchris@users.noreply.github.com>
2023-10-18 17:11:58 -0700

    changed deploy script to handle resume PNG instead of JPG

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ cd "$(dirname "$0")"
 # Font subset
 ./get-all-chars.py space.ttf
 
-# Get Resume JPG, convert to webp, and resize
-wget https://raw.githubusercontent.com/cohenchris/resume/master/ChrisCohen_resume.jpg
-convert ChrisCohen_resume.jpg -resize 798x1128 ChrisCohen_resume.jpg
-cwebp -q 50 ChrisCohen_resume.jpg -o static/images/ChrisCohen_resume.webp
-rm ChrisCohen_resume.jpg
+# Get Resume PNG, convert to webp, and resize
+wget https://raw.githubusercontent.com/cohenchris/resume/master/ChrisCohen_resume.png
+convert ChrisCohen_resume.png -resize 798x1128 ChrisCohen_resume.png
+cwebp -q 50 ChrisCohen_resume.png-o static/images/ChrisCohen_resume.webp
+rm ChrisCohen_resume.png
 
 # Sync music listening progress metadata
 python3 ./website-listening-progress-json.py
@@ -24,8 +24,8 @@ if [ "$1" != "test" ]; then
   rm -r /home/phrog/server/config/swag/www/chriscohen.dev/html
 
   mv html /home/phrog/server/config/swag/www/chriscohen.dev/
-
-  cd $starting_dir
 fi
+
+cd $starting_dir
 
 echo "Don't forget to commit!"
